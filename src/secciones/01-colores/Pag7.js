8e08bd0fecb9c5b1e419203907deebe57b923246
Facundo Leañez <facundo.leanez@hotmail.com>
2021-09-23 16:27:43 -0300

    fix

--- src/secciones/01-colores/Pag7.js
+++ src/secciones/01-colores/Pag7.js
@@ -6,39 +6,39 @@ export const Pag7 = () => {
             <h1 className="mt-5">PRACTIQUEMOS JUNTOS CON YHARI EL COLOR ROJO</h1>
             <div className="row">
                 <div className="col-6">
-                    <img  style={{height:"10rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Globo.png" />
+                    <img alt="imagen" style={{height:"10rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Globo.png" />
                 </div>
                 <div className="col-6  ">
                     <div className="bg-amarillo overflow-hidden p-2 pt-5 mx-5"  style={{height:350, borderRadius:"50%", width:350, overflow:"" }}>
-
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-amarillo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-azul.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-celeste.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-naranja.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-rojo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde-agua.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-amarillo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-azul.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-celeste.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-naranja.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-rojo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde-agua.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-amarillo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-azul.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-celeste.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-naranja.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-rojo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde-agua.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-amarillo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-azul.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-celeste.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-naranja.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-rojo.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde.png" />
-                            <img style={{height:"3rem"}} src="static\Elementosporseparado\2Coloresprimarios\Rojo\Corazon-verde-agua.png" />
+{/* 
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-amarillo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-azul.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-celeste.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-naranja.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-rojo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde-agua.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-amarillo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-azul.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-celeste.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-naranja.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-rojo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde-agua.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-amarillo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-azul.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-celeste.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-naranja.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-rojo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde-agua.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-amarillo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-azul.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-celeste.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-naranja.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-rojo.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde.png" />
+                            <img style={{height:"3rem"}} src="static/Elementosporseparado/2Coloresprimarios/Rojo/Corazon-verde-agua.png" /> */}
 
 
                     </div>
