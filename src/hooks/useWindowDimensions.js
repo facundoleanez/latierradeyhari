f42b2139888d62174c00a451bf13d60bf6ac9580
Facundo Leañez <facundo.leanez@hotmail.com>
2021-09-28 17:29:25 -0300

    layout

--- src/hooks/useWindowDimensions.js
+++ src/hooks/useWindowDimensions.js
@@ -29,7 +29,7 @@ export const responsiveWidth = (width) => {
   } else if (width <= 992) {
       return 480
   } else if (width <= 1600) {
-      return 960   
+      return 850  
   } else {
       return 1200
   }
@@ -40,7 +40,7 @@ export const responsiveHeight = (width) => {
   } else if (width <= 992) {
       return 270
   } else if (width <= 1600) {
-      return 540
+      return 475
   } else {
       return 675
   }
