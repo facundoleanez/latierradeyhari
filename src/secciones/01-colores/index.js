774a78b50c98db0269435ffd8d6372f1a03e6a64
Facundo Leañez <facundo.leanez@hotmail.com>
2021-10-12 15:29:27 -0300

    changed location

--- src/secciones/01-colores/index.js
+++ src/secciones/01-colores/index.js
@@ -1,6 +1,5 @@
 export { Pag4 } from './Pag4';
 export { Pag5 } from './Pag5';
-export { Pag7 } from './Pag7';
 export { Pag8 } from './Pag8';
 export { Pag10 } from './Pag10';
 export { Pag11 } from './Pag11';
